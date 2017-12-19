4b53f1a1039e7480424049225352d9b349a0d7cd
Justin Li <git@justinli.net>
2017-12-19 17:16:51 -0500

    Force correct index when selecting from sharded tables

--- reloc/test/table_filter_test.go
+++ reloc/test/table_filter_test.go
@@ -83,35 +83,3 @@ func TestShardedTableFilterSelectsJoinedTables(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, tables[1:], applicable)
 }
-
-func TestShardedRowFilterSupportsJoinedTables(t *testing.T) {
-	shardingValue := int64(1)
-	pkCursor := uint64(12345)
-
-	filter := &reloc.ShardedRowFilter{
-		ShardingKey:   "tenant_id",
-		ShardingValue: shardingValue,
-
-		JoinedTables: map[string][]reloc.JoinTable{
-			"joined": []reloc.JoinTable{
-				{TableName: "join1", JoinColumn: "joined_pk1"},
-				{TableName: "join2", JoinColumn: "joined_pk2"},
-			},
-		},
-	}
-
-	table := &schema.Table{
-		Schema:    "shard_1",
-		Name:      "joined",
-		Columns:   []schema.TableColumn{{Name: "joined_pk"}},
-		PKColumns: []int{0},
-	}
-
-	selectBuilder, err := filter.BuildSelect(table, pkCursor, 1024)
-	assert.Nil(t, err)
-
-	sql, args, err := selectBuilder.ToSql()
-	assert.Nil(t, err)
-	assert.Equal(t, "SELECT * FROM `shard_1`.`joined` WHERE `joined_pk` IN (SELECT * FROM (SELECT `joined_pk1` AS reloc_join_alias FROM `shard_1`.`join1` WHERE `tenant_id` = ? AND `joined_pk1` > ? UNION DISTINCT SELECT `joined_pk2` AS reloc_join_alias FROM `shard_1`.`join2` WHERE `tenant_id` = ? AND `joined_pk2` > ? ORDER BY reloc_join_alias LIMIT 1024) AS reloc_join_table) ORDER BY `joined_pk`", sql)
-	assert.Equal(t, []interface{}{shardingValue, pkCursor, shardingValue, pkCursor}, args)
-}
