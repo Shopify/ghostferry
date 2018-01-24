a1314619899a4a2dcadd3648da8ba65a1e4a9559
Hormoz K <hkheradm@gmail.com>
2018-01-24 08:31:01 -0500

    do not throttle during cutover copy phase

--- test/throttler_test.go
+++ test/throttler_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/Shopify/ghostferry"
 	"github.com/Shopify/ghostferry/testhelpers"
@@ -14,18 +15,49 @@ type ThrottlerTestSuite struct {
 	throttler ghostferry.Throttler
 }
 
-func (this *ThrottlerTestSuite) SetupTest() {
-	this.throttler = &ghostferry.PauserThrottler{}
+func (t *ThrottlerTestSuite) SetupTest() {
+	t.throttler = &ghostferry.PauserThrottler{}
 }
 
-func (this *ThrottlerTestSuite) TestPauseUnpause() {
-	this.Require().False(this.throttler.Throttled())
+func (t *ThrottlerTestSuite) TestPauseUnpause() {
+	t.Require().False(t.throttler.Throttled())
 
-	this.throttler.SetPaused(true)
-	this.Require().True(this.throttler.Throttled())
+	t.throttler.SetPaused(true)
+	t.Require().True(t.throttler.Throttled())
 
-	this.throttler.SetPaused(false)
-	this.Require().False(this.throttler.Throttled())
+	t.throttler.SetPaused(false)
+	t.Require().False(t.throttler.Throttled())
+}
+
+func (t *ThrottlerTestSuite) TestEnableDisable() {
+	t.throttler.SetPaused(true)
+	t.Require().False(t.throttler.Disabled())
+	t.Require().True(t.throttler.Throttled())
+
+	t.throttler.SetDisabled(true)
+	t.Require().True(t.throttler.Disabled())
+	ghostferry.WaitForThrottle(t.throttler)
+
+	t.throttler.SetDisabled(false)
+	t.Require().False(t.throttler.Disabled())
+
+	done := make(chan bool)
+	resumed := false
+	go func() {
+		ghostferry.WaitForThrottle(t.throttler)
+		resumed = true
+		done <- true
+	}()
+
+	time.Sleep(200 * time.Millisecond)
+	t.Require().False(resumed)
+
+	t.throttler.SetDisabled(true)
+	select {
+	case <-time.After(5 * time.Second):
+		t.Require().Fail("goroutine did not resume in time")
+	case <-done:
+	}
 }
 
 func TestThrottlerTestSuite(t *testing.T) {
