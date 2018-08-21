edb778951053bea1626d17671482d721c0e774a5
Shuhao Wu <shuhao@shuhaowu.com>
2018-08-21 12:16:56 -0400

    Fixed test issue

--- testhelpers/error_handler.go
+++ testhelpers/error_handler.go
@@ -4,6 +4,10 @@ type ErrorHandler struct {
 	LastError error
 }
 
+func (this *ErrorHandler) ReportError(from string, err error) {
+	this.Fatal(from, err)
+}
+
 func (this *ErrorHandler) Fatal(from string, err error) {
 	this.LastError = err
 }
