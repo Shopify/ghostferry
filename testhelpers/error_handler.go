e88c29551cfea77a57fca23859c0d8799a31fc81
Shuhao Wu <shuhao@shuhaowu.com>
2019-03-28 10:49:15 -0400

    Moved error handler atomic check to Fatal

--- testhelpers/error_handler.go
+++ testhelpers/error_handler.go
@@ -4,9 +4,8 @@ type ErrorHandler struct {
 	LastError error
 }
 
-func (this *ErrorHandler) ReportError(from string, err error) bool {
+func (this *ErrorHandler) ReportError(from string, err error) {
 	this.Fatal(from, err)
-	return true
 }
 
 func (this *ErrorHandler) Fatal(from string, err error) {
