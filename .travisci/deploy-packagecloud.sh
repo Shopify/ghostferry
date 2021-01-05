4778a4c159a08be6fdc2927c7b30f603d75ad105
Jean Boussier <jean.boussier@gmail.com>
2021-01-05 12:51:30 +0100

    Update the list of package cloud ubuntu dists

--- .travisci/deploy-packagecloud.sh
+++ .travisci/deploy-packagecloud.sh
@@ -7,6 +7,6 @@ gem install package_cloud
 packagecloud_url="https://packages.shopify.io"
 repo="shopify/ghostferry"
 
-for dist in {ubuntu/trusty,ubuntu/xenial}; do
+for dist in {ubuntu/trusty,ubuntu/xenial,ubuntu/bionic,ubuntu/cosmic}; do
   package_cloud push --url "$packagecloud_url" "$repo/$dist" build/ghostferry-*.deb
 done
