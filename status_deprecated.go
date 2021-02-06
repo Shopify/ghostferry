fe498d2bb3decb55ade9bbe2cb7fbe04ebbefb60
Manan007224 <maniyarmanan1996@gmail.com>
2021-02-05 17:14:38 -0800

    refactoring status to use progress

--- status_deprecated.go
+++ status_deprecated.go
@@ -2,7 +2,6 @@ package ghostferry
 
 import (
 	"fmt"
-	"math"
 	"sort"
 	"time"
 
@@ -12,7 +11,7 @@ import (
 // NOTE: This file is only used for the ControlServer for now.
 // TODO: eventually merge this into the ControlServer and use the Progress struct.
 
-type TableStatusDeprecated struct {
+type TableStatus struct {
 	TableName                   string
 	PaginationKeyName           string
 	Status                      string
@@ -20,7 +19,7 @@ type TableStatusDeprecated struct {
 	TargetPaginationKey         uint64
 }
 
-type StatusDeprecated struct {
+type Status struct {
 	GhostferryVersion string
 
 	SourceHostPort string
@@ -43,7 +42,7 @@ type StatusDeprecated struct {
 
 	CompletedTableCount int
 	TotalTableCount     int
-	TableStatuses       []*TableStatusDeprecated
+	TableStatuses       []*TableStatus
 	AllTableNames       []string
 	AllDatabaseNames    []string
 
@@ -56,8 +55,9 @@ type StatusDeprecated struct {
 	VerificationErr     error
 }
 
-func FetchStatusDeprecated(f *Ferry, v Verifier) *StatusDeprecated {
-	status := &StatusDeprecated{}
+func FetchStatus(f *Ferry, v Verifier) *Status {
+	progress := f.Progress()
+	status := &Status{}
 
 	status.GhostferryVersion = VersionString
 
@@ -65,134 +65,57 @@ func FetchStatusDeprecated(f *Ferry, v Verifier) *StatusDeprecated {
 	status.TargetHostPort = fmt.Sprintf("%s:%d", f.Target.Host, f.Target.Port)
 
 	status.OverallState = fmt.Sprintf("%s", f.OverallState.Load())
+
 	status.StartTime = f.StartTime
 	status.CurrentTime = time.Now()
-	if f.DoneTime.IsZero() {
-		status.TimeTaken = status.CurrentTime.Sub(status.StartTime)
-	} else {
-		status.TimeTaken = f.DoneTime.Sub(status.StartTime)
-	}
-	status.BinlogStreamerLag = time.Now().Sub(f.BinlogStreamer.lastProcessedEventTime)
+	status.TimeTaken = time.Duration(progress.TimeTaken)
+
+	status.BinlogStreamerLag = time.Duration(progress.BinlogStreamerLag)
+	status.LastSuccessfulBinlogPos = progress.LastSuccessfulBinlogPos
+	status.TargetBinlogPos = progress.FinalBinlogPos
 
 	status.AutomaticCutover = f.Config.AutomaticCutover
 	status.BinlogStreamerStopRequested = f.BinlogStreamer.stopRequested
-	status.LastSuccessfulBinlogPos = f.BinlogStreamer.lastStreamedBinlogPosition
-	status.TargetBinlogPos = f.BinlogStreamer.stopAtBinlogPosition
 
-	status.Throttled = f.Throttler.Throttled()
+	status.Throttled = progress.Throttled
 
 	// Getting all table statuses
-	status.TableStatuses = make([]*TableStatusDeprecated, 0, len(f.Tables))
-
-	serializedState := f.StateTracker.Serialize(nil, nil)
-
-	lastSuccessfulPaginationKeys := serializedState.LastSuccessfulPaginationKeys
-	completedTables := serializedState.CompletedTables
-
-	targetPaginationKeys := make(map[string]uint64)
-	f.DataIterator.targetPaginationKeys.Range(func(k, v interface{}) bool {
-		targetPaginationKeys[k.(string)] = v.(uint64)
-		return true
-	})
-
-	status.CompletedTableCount = len(completedTables)
-	status.TotalTableCount = len(f.Tables)
-
-	status.AllTableNames = f.Tables.AllTableNames()
-	sort.Strings(status.AllTableNames)
+	status.TableStatuses = make([]*TableStatus, 0, len(f.Tables))
+	status.CompletedTableCount = 0
+	status.TotalTableCount = len(progress.Tables)
+	status.AllTableNames = make([]string, 0, len(progress.Tables))
 
 	dbSet := make(map[string]bool)
-	for _, table := range f.Tables.AsSlice() {
-		dbSet[table.Schema] = true
+	for name, tableProgress := range progress.Tables {
+		status.TableStatuses = append(status.TableStatuses, &TableStatus{
+			TableName:                   name,
+			PaginationKeyName:           f.Tables[name].GetPaginationColumn().Name,
+			Status:                      tableProgress.CurrentAction,
+			LastSuccessfulPaginationKey: tableProgress.LastSuccessfulPaginationKey,
+			TargetPaginationKey:         tableProgress.LastSuccessfulPaginationKey,
+		})
+
+		status.AllTableNames = append(status.AllTableNames, name)
+		dbSet[f.Tables[name].Schema] = true
+
+		if tableProgress.CurrentAction == TableActionCompleted {
+			status.CompletedTableCount++
+		}
 	}
 
 	status.AllDatabaseNames = make([]string, 0, len(dbSet))
 	for dbName := range dbSet {
 		status.AllDatabaseNames = append(status.AllDatabaseNames, dbName)
 	}
+
 	sort.Strings(status.AllDatabaseNames)
-
-	// We get the name first because we need to sort them
-	completedTableNames := make([]string, 0, len(completedTables))
-	copyingTableNames := make([]string, 0, len(f.Tables))
-	waitingTableNames := make([]string, 0, len(f.Tables))
-
-	for tableName, _ := range completedTables {
-		completedTableNames = append(completedTableNames, tableName)
-	}
-
-	for tableName, _ := range lastSuccessfulPaginationKeys {
-		if _, ok := completedTables[tableName]; ok {
-			continue // already completed, therefore not copying
-		}
-
-		copyingTableNames = append(copyingTableNames, tableName)
-	}
-
-	for tableName, _ := range f.Tables {
-		if lastSuccessfulPaginationKey, ok := lastSuccessfulPaginationKeys[tableName]; ok && lastSuccessfulPaginationKey != 0 {
-			continue // already started, therefore not waiting
-		}
-
-		if _, ok := completedTables[tableName]; ok {
-			// There are no data in that table, thus it does not have an entry in
-			// lastSuccessfulPaginationKeys but has an entry in completedTables
-			continue
-		}
-
-		waitingTableNames = append(waitingTableNames, tableName)
-	}
-
-	sort.Strings(completedTableNames)
-	sort.Strings(copyingTableNames)
-	sort.Strings(waitingTableNames)
-
-	for _, tableName := range completedTableNames {
-		status.TableStatuses = append(status.TableStatuses, &TableStatusDeprecated{
-			TableName:                   tableName,
-			PaginationKeyName:           f.Tables[tableName].GetPaginationColumn().Name,
-			Status:                      "complete",
-			TargetPaginationKey:         targetPaginationKeys[tableName],
-			LastSuccessfulPaginationKey: lastSuccessfulPaginationKeys[tableName],
-		})
-	}
-
-	for _, tableName := range copyingTableNames {
-		status.TableStatuses = append(status.TableStatuses, &TableStatusDeprecated{
-			TableName:                   tableName,
-			PaginationKeyName:           f.Tables[tableName].GetPaginationColumn().Name,
-			Status:                      "copying",
-			TargetPaginationKey:         targetPaginationKeys[tableName],
-			LastSuccessfulPaginationKey: lastSuccessfulPaginationKeys[tableName],
-		})
-	}
-
-	for _, tableName := range waitingTableNames {
-		status.TableStatuses = append(status.TableStatuses, &TableStatusDeprecated{
-			TableName:                   tableName,
-			PaginationKeyName:           f.Tables[tableName].GetPaginationColumn().Name,
-			Status:                      "waiting",
-			TargetPaginationKey:         targetPaginationKeys[tableName],
-			LastSuccessfulPaginationKey: 0,
-		})
-	}
+	sort.Strings(status.AllTableNames)
 
 	// ETA estimation
 	// We do it here rather than in DataIteratorState to give the lock back
 	// ASAP. It's not supposed to be that accurate anyway.
-	var totalPaginationKeysToCopy uint64 = 0
-	var completedPaginationKeys uint64 = 0
-	estimatedPaginationKeysPerSecond := f.StateTracker.EstimatedPaginationKeysPerSecond()
-	for _, targetPaginationKey := range targetPaginationKeys {
-		totalPaginationKeysToCopy += targetPaginationKey
-	}
-
-	for _, completedPaginationKey := range lastSuccessfulPaginationKeys {
-		completedPaginationKeys += completedPaginationKey
-	}
-
-	status.ETA = time.Duration(math.Ceil(float64(totalPaginationKeysToCopy-completedPaginationKeys)/estimatedPaginationKeysPerSecond)) * time.Second
-	status.PaginationKeysPerSecond = uint64(estimatedPaginationKeysPerSecond)
+	status.ETA = time.Duration(progress.ETA)
+	status.PaginationKeysPerSecond = progress.PaginationKeysPerSecond
 
 	// Verifier display
 	if v != nil {
