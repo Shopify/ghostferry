70599d2cc0716b6636e94d9ec4a93ac019c6903e
Shuhao Wu <shuhao@shuhaowu.com>
2021-04-28 15:48:08 +0000

    Added github pages

---
+++ .github/workflows/build-docs.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -xe
+
+sudo apt-get update
+sudo apt-get install -y python3-pip
+sudo pip3 install sphinx==1.8.5
+cd docs
+make html
+
+cd build
+git clone --depth 1 https://github.com/Shopify/ghostferry.git -b gh-pages ghostferry-pages
+current_branch=${GITHUB_REF#refs/heads/}
+cp -ar html/. ghostferry-pages/${current_branch}
+cd ghostferry-pages
+
+echo "<html>" > index.html
+echo "  <head>" >> index.html
+echo "    <title>Ghostferry Documentations</title>" >> index.html
+echo "  </head>" >> index.html
+echo "  <body>" >> index.html
+echo "    <h3>Ghostferry Documentation Version Selector</h3>" >> index.html
+echo "    <ul>" >> index.html
+for d in */; do
+echo "      <li><a href=\"${d}index.html\">${d}</a></li>" >> index.html
+done
+echo "    </ul>" >> index.html
+echo "  </body>" >> index.html
+echo "</html>" >> index.html
+
+git status
+
+cd ../../..
+ls -la docs/build/ghostferry-pages
+rm .git -rf
