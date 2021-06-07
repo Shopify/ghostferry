99283fb4f505c37cc365b2678643ffdc6a01bb2c
William Tisäter <william.tisater@shopify.com>
2021-06-07 21:16:31 +0200

    Filter MD5 column smarter

--- data_iterator.go
+++ data_iterator.go
@@ -130,7 +130,7 @@ func (d *DataIterator) Run(tables []*TableSchema) {
 							paginationKeyIndex: batch.PaginationKeyIndex(),
 							table:              table,
 							fingerprints:       fingerprints,
-							columns:            batch.columns,
+							columns:            batch.columns[:len(batch.columns)-1],
 						}
 					}
 
