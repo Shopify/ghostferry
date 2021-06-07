79c1825e23d7a6ee75297728ec28caf9b1bd6c15
William Tisäter <william.tisater@shopify.com>
2021-06-07 20:34:29 +0200

    Run GH-285 repro on CI

---
+++ examples/gh-285/bugreport.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+docker-compose up -d
+
+mysql -h 127.0.0.1 -u root -P 29291 -e 'DROP DATABASE `abc`'
+mysql -h 127.0.0.1 -u root -P 29292 -e 'DROP DATABASE `abc`'
+
+mysql -h 127.0.0.1 -u root -P 29291 -e 'CREATE DATABASE `abc`'
+mysql -h 127.0.0.1 -u root -P 29292 -e 'CREATE DATABASE `abc`'
+
+mysql -h 127.0.0.1 -u root -P 29291 -e 'CREATE TABLE `abc`.`t` (`tenant_id` bigint(20) NOT NULL, `col1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `id` bigint(20) NOT NULL AUTO_INCREMENT, `d` datetime(6) NOT NULL, PRIMARY KEY (`tenant_id`,`col1`), KEY `id` (`id`))'
+mysql -h 127.0.0.1 -u root -P 29292 -e 'CREATE TABLE `abc`.`t` (`tenant_id` bigint(20) NOT NULL, `col1` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL, `id` bigint(20) NOT NULL AUTO_INCREMENT, `d` datetime(6) NOT NULL, PRIMARY KEY (`tenant_id`,`col1`), KEY `id` (`id`))'
+
+mysql -h 127.0.0.1 -u root -P 29291 -e 'INSERT INTO `abc`.`t` VALUES (1, '\''z'\'', NULL, NOW())'
+mysql -h 127.0.0.1 -u root -P 29291 -e 'INSERT INTO `abc`.`t` VALUES (1, '\''a'\'', NULL, NOW())'
+
+ghostferry-sharding -config-path "$(cd -P "$(dirname "$0")" && pwd)/config.json"
+
+SOURCE=$(mysql -u root -h 127.0.0.1 -P 29291 -e 'SELECT * FROM `abc`.`t`')
+TARGET=$(mysql -u root -h 127.0.0.1 -P 29292 -e 'SELECT * FROM `abc`.`t`')
+
+if [[ "$SOURCE" != "$TARGET" ]]; then
+	echo "Source table did not match target"
+	echo
+	echo "Source:"
+	echo "$SOURCE"
+	echo
+	echo "Target:"
+	echo "$TARGET"
+	exit 1
+fi
