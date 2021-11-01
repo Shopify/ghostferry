e13e0a1d2ef83bf88e64e3f1cdd76fd9e33ca56c
Pawan Dubey <pawan.dubey@shopify.com>
2021-11-01 11:43:00 -0400

    WIP type change and rename tests

--- sharding/test/schema_change_integration_test.go
+++ sharding/test/schema_change_integration_test.go
@@ -82,3 +82,75 @@ func TestDroppingColumnOnSourceBeforeTargetWorks(t *testing.T) {
 
 	assert.True(t, count > 0)
 }
+
+func TestChangingTypeOnSourceWorks(t *testing.T) {
+	testcase := &testhelpers.IntegrationTestCase{
+		T:                       t,
+		Ferry:                   testhelpers.NewTestFerry(),
+		DisableChecksumVerifier: true,
+		SetupAction:             setupSingleTableDatabase,
+		DataWriter: &testhelpers.MixedActionDataWriter{
+			ProbabilityOfInsert: 1.0,
+			NumberOfWriters:     2,
+			Tables:              []string{"gftest.table1"},
+		},
+		AfterStartBinlogStreaming: func(f *testhelpers.TestFerry, sourceDB *sql.DB, targetDB *sql.DB) {
+			_, err := sourceDB.Exec("ALTER TABLE gftest.table1 MODIFY data VARCHAR(255)")
+			testhelpers.PanicIfError(err)
+		},
+	}
+
+	defer testcase.Teardown()
+	testcase.CopyData()
+
+	testcase.AssertQueriesHaveEqualResult("SELECT count(*) FROM gftest.table1")
+
+	var count int
+	row := testcase.Ferry.TargetDB.QueryRow("SELECT count(*) FROM gftest.table1")
+	testhelpers.PanicIfError(row.Scan(&count))
+
+	assert.True(t, count > 0)
+}
+
+func TestRenamingTableOnSourceOrig(t *testing.T) {
+	errorHandler := &testhelpers.ErrorHandler{}
+	ferry := testhelpers.NewTestFerry()
+	ferry.ErrorHandler = errorHandler
+	testcase := &testhelpers.IntegrationTestCase{
+		T:                       t,
+		Ferry:                   ferry,
+		DisableChecksumVerifier: true,
+		SetupAction:             setupSingleTableDatabase,
+		DataWriter: &testhelpers.MixedActionDataWriter{
+			ProbabilityOfInsert: 1.0,
+			NumberOfWriters:     2,
+			Tables:              []string{"gftest.table1"},
+		},
+		AfterStartBinlogStreaming: func(f *testhelpers.TestFerry, sourceDB *sql.DB, targetDB *sql.DB) {
+			_, err := sourceDB.Exec("ALTER TABLE gftest.table1 CHANGE data data_new TEXT")
+			fmt.Printf("ERROR: %v\n", err)
+			testhelpers.PanicIfError(err)
+		},
+	}
+
+	defer testcase.Teardown()
+
+	// defer func() {
+	// 	if r := recover(); r == nil {
+	// 		t.Errorf("Did not fail as expected")
+	// 	}
+	// }()
+
+	// fmt.Println("starting copy")
+	//assert.Panics(t, func() { panic("lol") })
+	//assert.Panics(t, testcase.CopyData)
+	assert.Panics(t, func() { testcase.CopyData() })
+
+	// assert.PanicsWithValue(t, nil, testcase.CopyData)
+
+	// assert.Equal(t, "Unknown column 'data' in 'field list'", errorHandler.LastError)
+}
+
+func TestRenamingTableOnSourceFails(t *testing.T) {
+	assert.Panics(t, func() { panic("lol") })
+}
